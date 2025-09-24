698c7e999f174998c2f40c11e55d5fba66dc62a0
karth <karth@karthi>
2025-09-24 20:29:27 +0530

    commit

--- src/core_javaprogram/day1.java
+++ src/core_javaprogram/day1.java
@@ -4,7 +4,7 @@ public class day1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("you can done coders ");
+        System.out.println("do it again");
 	}
 
 }
