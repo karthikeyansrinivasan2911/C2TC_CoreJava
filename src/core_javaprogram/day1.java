f1ec00f122a357a5a098fc0944c3e2eda08fc22b
karth <karth@karthi>
2025-09-26 16:04:25 +0530

    +- java project

--- src/core_javaprogram/day1.java
+++ src/core_javaprogram/day1.java
@@ -4,7 +4,7 @@ public class day1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("do it know");
+        System.out.println("do it now");
 	}
 
 }
