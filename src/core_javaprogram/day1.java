9bacf31f6f27c3e627e84e23d6290ed1ba41e284
karth <karth@karthi>
2025-09-29 10:13:49 +0530

    commit next day

--- src/core_javaprogram/day1.java
+++ src/core_javaprogram/day1.java
@@ -5,7 +5,7 @@ public class day1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("do it now");
-        System.out.println("welcome to github");
+        System.out.println("welcome to next day");
 	}
 
 }
