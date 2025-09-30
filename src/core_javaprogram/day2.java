f9eb9d159836eb738970085243f627f55c1a202c
karth <karth@karthi>
2025-09-30 11:26:17 +0530

    commit two

---
+++ src/core_javaprogram/day2.java
@@ -0,0 +1,13 @@
+package core_javaprogram;
+
+public class day2 {
+	public static void main(String[] args) {
+		System.out.println("main method");
+		main(20);
+		}
+     public static void main(int a) {
+    	 System.out.print("overloaded main method"+ a);
+    	  
+    	 
+     }
+}
